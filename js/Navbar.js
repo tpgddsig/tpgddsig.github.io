4c00e4caf03866e704c93bbd08ed11775fdf842a
Diffract <46133904+Diffracto@users.noreply.github.com>
2022-05-19 04:30:49 +0800

    Light and Dark mode

---
+++ js/Navbar.js
@@ -0,0 +1,14 @@
+﻿var navbar = {};
+
+navbar.darkModeSwitch = function (){
+    var icon = document.getElementById("theme-toggle-icon");
+    console.log("hi!");
+    document.body.classList.toggle("dark-mode");
+    
+    if(document.body.classList.contains("dark-mode")){
+        icon.src = "images/sun.png";
+    }
+    else{
+        icon.src = "images/moon.png";
+    }
+}
