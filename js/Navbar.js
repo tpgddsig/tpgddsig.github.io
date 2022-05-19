0cd9ed8f2a12ef4cf9ae5e1ede43c02b5965661b
Astero <adenv3@gmail.com>
2022-05-20 06:07:44 +0800

    Added navbar-items

--- js/Navbar.js
+++ js/Navbar.js
@@ -2,8 +2,11 @@
 
 navbar.darkModeSwitch = function (){
     var icon = document.getElementById("theme-toggle-icon");
-    console.log("hi!");
+    
     document.body.classList.toggle("dark-mode");
+
+    console.log(document.body.classList);
+   
     
     if(document.body.classList.contains("dark-mode")){
         icon.src = "./Images/sun.png";
