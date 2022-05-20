c7e83dc3b301b8661f6cba6f235dad5e81c97377
Astero <adenv3@gmail.com>
2022-05-20 15:31:47 +0800

    Removed debugs

--- js/index.js
+++ js/index.js
@@ -3,15 +3,14 @@
 function OnBodyLoad()
 {
     const darkMode = localStorage.getItem("dark-mode");
-    
-    console.log(darkMode === true);
+
     
     if(darkMode != null)
     {
-        if(darkMode == "true")
+        if(darkMode === "true")
         {
             navbar.darkModeSwitch();
-            console.log("ya");
+     
         }
     }
 }
