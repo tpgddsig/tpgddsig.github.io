4388f39b96310a6881670e2827baef5c59ea9946
Astero <adenv3@gmail.com>
2022-05-23 05:16:00 +0800

    WIP Events Page

---
+++ events/js/events.js
@@ -0,0 +1,25 @@
+
+
+function EventMouseOver(id)
+{
+
+    
+    
+    id.children[0].firstElementChild.children[2].firstElementChild.firstElementChild.style.display = "inline-block";
+    
+}
+
+function EventMouseDown(id)
+{
+
+
+
+    id.children[0].firstElementChild.children[2].firstElementChild.firstElementChild.style.display = "none";
+    
+
+    //id.children[1].classList.remove("event-container-blur");
+    
+    
+    //console.log("down");
+
+}
